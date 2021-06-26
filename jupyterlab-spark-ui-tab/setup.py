6a0146312a0f60eda3f785530e562f98f35bbd97
chrispyl <pylianidis@gmail.com>
2021-06-26 17:40:08 +0200

    update

--- jupyterlab-spark-ui-tab/setup.py
+++ jupyterlab-spark-ui-tab/setup.py
@@ -5,23 +5,23 @@ from subprocess import check_output
 
 
 
-setup(name='jupyterlab-spark-ui-tab',
-      version= check_output("git describe --tags".split()).decode("utf-8").strip(),
-      description='Spark UI extension for jupyterlab',
-      author='Lior Baber',
-      author_email='liorbaber@gmail.com',
-      include_package_data=True,
-      packages=find_packages(),
-      license="apache-2.0",
-      url = 'https://github.com/Liorba/jupyterlab-spark-ui-tab',
-      keywords = ['jupyter', 'extension', 'spark'],
-      classifiers = [
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: Apache Software License',
-      'Programming Language :: Python :: 3',
-          ],
-      zip_safe=False,
-      install_requires=[
-          'bs4' ,
-      ]
-      )
+# setup(name='jupyterlab-spark-ui-tab',
+      # version= check_output("git describe --tags".split()).decode("utf-8").strip(),
+      # description='Spark UI extension for jupyterlab',
+      # author='Lior Baber',
+      # author_email='liorbaber@gmail.com',
+      # include_package_data=True,
+      # packages=find_packages(),
+      # license="apache-2.0",
+      # url = 'https://github.com/Liorba/jupyterlab-spark-ui-tab',
+      # keywords = ['jupyter', 'extension', 'spark'],
+      # classifiers = [
+      # 'Intended Audience :: Developers',
+      # 'License :: OSI Approved :: Apache Software License',
+      # 'Programming Language :: Python :: 3',
+          # ],
+      # zip_safe=False,
+      # install_requires=[
+          # 'bs4' ,
+      # ]
+      # )
